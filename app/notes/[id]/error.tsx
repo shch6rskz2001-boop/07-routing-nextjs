3dc6731a0236341648655812d055259eb6331af9
shch6rskz2001-boop <shch6rskz2001@gmail.com>
2026-05-25 23:06:47 +0300

    fleks

--- app/notes/[id]/error.tsx
+++ app/notes/[id]/error.tsx
@@ -9,11 +9,12 @@ export default function NoteDetailsError({ error, reset }: ErrorProps) {
   return (
     <div>
       <p>Could not fetch note details. {error.message}</p>
-      {/* Кнопка використовує reset, і ESLint більше не свариться */}
       <button onClick={() => reset()}>Try again</button>
     </div>
   );
 }
+
+
 
 
 
