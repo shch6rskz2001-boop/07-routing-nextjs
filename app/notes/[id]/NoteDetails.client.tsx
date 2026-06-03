18ba24353a5d5e4ea85579732f334e09b2990ed6
shch6rskz2001-boop <shch6rskz2001@gmail.com>
2026-06-03 14:50:19 +0300

    fleks

---
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { useQuery } from '@tanstack/react-query';
+import { fetchNoteById } from '@/lib/api';
+
+interface Props {
+  id: string;
+}
+
+export default function NoteDetailsClient({ id }: Props) {
+  const { data: note, isLoading, isError } = useQuery({
+    queryKey: ['note', id],
+    queryFn: () => fetchNoteById(id),
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (isError || !note) return <p>Note not found.</p>;
+
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>{note.title}</h1>
+      {note.tag && <span>{note.tag}</span>}
+      <p>{note.content}</p>
+      <time>{new Date(note.createdAt).toLocaleDateString('uk-UA')}</time>
+    </div>
+  );
+}
